d9a656c02f0c7133bf0d76959598a7f80f8d40ca
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2022-09-08 23:10:05 +0200

    created file structure

---
+++ R/ProprocessGeodata.R
@@ -0,0 +1,4 @@
+# Food aid distributed to Ukrainian towns and villages 
+# Commossioned by Worldparners
+# Excecuded by Niek Koelewijn
+# September 2022
--- main.R
+++ main.R
@@ -1,4 +1,25 @@
-# Food aid distributed to Ukrainian towns and cities 
-# World Parners
+# Food aid distributed to Ukrainian towns and villages 
+# Commossioned by Worldparners
+# Excecuded by Niek Koelewijn
 # September 2022
-# By Niek Koelewijn
+
+## This project aims to map the towns and villages in Ukraine that are received
+## food aid from the NGO Worldpartners. The maps should have the following 
+## requirements: (1) visualize which towns and villages are aided by which
+## distributions centers, (2) the number of aided families (probably per 
+## village), and (3) the quantity and type of food delivered to the villages.
+
+## The project will be carried out in three steps: (1) preprocessing of the
+## input data from Worldpartners, (2)  preprocessing of other needed geo-data,
+## and (3) creating the visualizations in leaflet
+
+setwd("~/WorldPartnersHelp")
+
+# Step 1: Preprocessing the input data from Worldpartners
+source("~/WorldPartnersHelp/R/PreprocessWorldpartnersData.R")
+
+# Step 2: Preprocessing other relevant geo-data
+source("~/WorldPartnersHelp//R/ProprocessGeodata.R")
+
+# Step 3: Creating the visualization
+source("~/WorldPartnersHelp//R/Visualization.R")
